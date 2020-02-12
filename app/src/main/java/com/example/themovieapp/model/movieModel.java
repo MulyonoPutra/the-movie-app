e552fce4c13c87174e87bf06b72455f02e2e1868
clara_p <clara_p@photoninfotech.net>
2020-02-12 20:31:04 +0700

    add movie model

---
+++ app/src/main/java/com/example/themovieapp/model/movieModel.java
@@ -0,0 +1,23 @@
+package com.example.themovieapp.model;
+
+public class movieModel {
+
+    private String movieTitle;
+    private String moviePoster;
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public String getMoviePoster() {
+        return moviePoster;
+    }
+
+    public void setMoviePoster(String moviePoster) {
+        this.moviePoster = moviePoster;
+    }
+}
